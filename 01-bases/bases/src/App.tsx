deb8d54dfd9504cb103ba5e256a3f6192418b784
Luis Miguel Alvarez <lmiguelalvc@gmail.com>
2025-09-30 20:52:31 -0500

    Clase 2

--- 01-bases/bases/src/App.tsx
+++ 01-bases/bases/src/App.tsx
@@ -1,18 +1,29 @@
 import './App.css'
+//import { BasicTypes } from './typescript/BasicTypes'
+//import BasicFunctions from './typescript/BasicFunctions'
+//import { ObjectLiterals } from './typescript/ObjectLiterals'
+// import { Counter } from './components/Counter'
+import reactLogo from './assets/react.svg'
 import { Counter } from './components/Counter'
-// import { ObjectLiterals } from './typescript/ObjectLiterals'
-// import { BasicTypes } from './typescript/BasicTypes'
-// import { BasicFunctions } from './typescript/BasicFunctions'
+import { Login } from './components/Login'
+import { AuthProvider } from './Context/AuthContext'
 function App() {
 
   return (
-    <>
-      <h1>React + TypeScript Reforzamiento</h1>
-      {/* <BasicFunctions /> */}
-      {/* <BasicTypes /> */}
-      {/* <ObjectLiterals /> */}
-      <Counter />
-    </>
+    //aqui se envuelve todo con el authprovider que fue lo que creamos
+    <AuthProvider>
+      <div className='flex flex-col justify-center items-center h-svh'>
+        <a href='https://es.react.dev' target='blank'></a>
+        <img src={reactLogo} className='w-32 h-32' alt='React Logo' />
+        <h1 className='text-4xl mb-5'>
+          React +
+          TypeScript Reforzamiento
+
+        </h1>
+        <Counter />
+        <Login />
+      </div>
+    </AuthProvider>
   )
 }
 
--- 01-bases/bases/src/components/Counter.tsx
+++ 01-bases/bases/src/components/Counter.tsx
@@ -1,13 +1,20 @@
+import { useAuthContext } from "../Context/AuthContext";
 import { useCounter } from "../hooks/useCounter";
 
 export const Counter = () => {
     const { count, increaseBy } = useCounter();
+    const { token } = useAuthContext();
     return (
         <>
-            <h3>Contador: <small>{count}</small></h3>
+            <h3 className="text-2">Contador: <small className="font-bold">{count}</small></h3>
+            <span>Token: {token}</span>
             <div>
-                <button onClick={() => increaseBy(1)}>+1</button>
-                <button onClick={() => increaseBy(-1)}>-1</button>
+                <button
+                    className="p-2 bg-blue-500 rounded-xl w-10 mx-2 text-white hover:bg-blue-700"
+                    onClick={() => increaseBy(1)}>+1</button>
+                <button
+                    className="p-2 bg-blue-500 rounded-xl w-10 mx-2 text-white hover:bg-blue-700"
+                    onClick={() => increaseBy(-1)}>-1</button>
             </div>
         </>
     )
